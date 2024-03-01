3c718252623b19ee22692a13a5a80e2473ff33bf
Nancyy0912 <nancy@simformsolutions.com>
2024-03-01 15:01:08 +0530

    Added info in Readme file

--- header/vite.config.js
+++ header/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Federation from '@originjs/vite-plugin-federation'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3003,
